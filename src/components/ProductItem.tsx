e6e395dac5631e95b4da27a223ed11a56e6d4219
mardsonferreira <mardsonferreira25@gmail.com>
2022-01-31 20:26:24 -0300

    Applying Memo on product item

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,17 +1,26 @@
+import { memo } from 'react';
+
 type Product = {
-  id: number;
-  price: number;
-  title: string;
+    id: number;
+    price: number;
+    title: string;
 };
 
 interface ProductItemProps {
-  product: Product;
+    product: Product;
 }
 
-export function ProductItem({ product }: ProductItemProps) {
-  return (
-    <div>
-      {product.title} - <strong>{product.price}</strong>
-    </div>
-  );
+function ProductItemComponent({ product }: ProductItemProps) {
+    return (
+        <div>
+            {product.title} - <strong>{product.price}</strong>
+        </div>
+    );
 }
+
+export const ProductItem = memo(
+    ProductItemComponent,
+    (prevProps, nextProps) => {
+        return Object.is(prevProps.product, nextProps.product);
+    }
+);
